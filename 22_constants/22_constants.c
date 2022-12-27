70a1d80d45938837de969663c08ceee36788885d
ITWorks4U <ITWorks4U@gmail.com>
2022-12-27 21:22:33 +0100

    constants in C

---
+++ 22_constants/22_constants.c
@@ -0,0 +1,44 @@
+/*
+	in C/C++ there're
+	tree different ways
+	to create and use
+	constants, whereas
+	macros are often
+	used for a
+	constant expression
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+	1st way:	using const keyword
+
+	fun fact:	in C a constant expression
+				can also have no value
+				in C++: it's illegal
+*/
+const int	expression_const = 123;
+
+/*
+	2nd way:	using macro
+*/
+#define		expression_macro	123
+
+/*
+	3rd way:	using anonymous enumeration
+				rareley used
+*/
+
+enum {
+	expression_enum = 123
+};
+
+int main() {
+	printf(
+		"%d, %d, %d\n",
+		expression_const, expression_macro, expression_enum
+	);
+
+	return EXIT_SUCCESS;
+}
